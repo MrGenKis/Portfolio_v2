9f7e402c7086d232ce1438999ddf6f5bae243068
MrGenKis <idriss.askri@hotmail.com>
2024-11-03 19:56:07 +0100

    Ajout de dotenv dans les dépendSDFHJBBHJHJKBDs

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ app.use(express.static(path.resolve(__dirname, '../Client/build')));
 
 // Gère toutes les autres routes en renvoyant l'index.html de React
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../Client/build', 'index.html'), function (err) {
+  res.sendFile(path.resolve(__dirname, '../Client/build/', 'index.html'), function (err) {
     if (err) {
       res.status(500).send(err);
     }
